d03a6ae4f3a32152d962ab568c440d901422e55a
Ian Enders <ian.enders@gmail.com>
2011-06-30 23:22:58 -0400

    No cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,10 @@ server domain, :app, :web
 role :db, domain, :primary => true
 
 namespace :deploy do
+  task :after_update_code do
+    link_up_cache
+  end
+
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
@@ -25,7 +29,15 @@ namespace :deploy do
   task :symlink_db do
     run "ln -s #{deploy_to}/#{shared_dir}/db/production.sqlite3 #{release_path}/db/production.sqlite3" 
   end
+
+  desc "Symlink in CMS upload directories to the current release"
+  task :link_up_cache do
+    run <<-CMD
+    ln -nfs #{shared_path}/stylesheet_cache #{release_path}/public/stylesheets/cache &&
+    ln -nfs #{shared_path}/javascript_cache #{release_path}/public/javascripts/cache
+    CMD
+  end
+
 end
 
-before "deploy:migrate", "deploy:load_releases"
 after "deploy:update_code", "deploy:symlink_db"
